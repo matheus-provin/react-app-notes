dae90e5cafe95e5152bfbdd3d90b07f55e84d9a6
matheus-provin <matheusprovinehlert@gmail.com>
2023-04-26 13:42:55 -0300

    ass

--- reactnotesfirebase/src/routes/routes.js
+++ reactnotesfirebase/src/routes/routes.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {HashRouter, Route, Routes} from 'react-router-dom'
 import Login from '../pages/Login'
 import Home from '../pages/Home'
 import { PrivateRoutes } from '.'
@@ -6,13 +6,13 @@ import { PrivateRoutes } from '.'
 
 export const AppRoutes = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path='/' element={<Login />}/> 
                 <Route path='/home' element={<PrivateRoutes />}>
                 <Route path='/home' element={<Home/>}/> 
                 </Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
